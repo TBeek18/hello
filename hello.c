35857bf3b9ff9917020d2b47cf82279b353fbab1
Trentyn Beekman <tbeekman2@huskers.unl.edu>
2021-08-26 16:27:03 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/** 
+ * Author: Trentyn Beekman
+ * Date: 2021/08/26
+ * 
+ * This program will print my name.
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Trentyn Beekman\n");
+     
+     return 0;
+     
+ }
