298c375b33094bb8e3d4377f2a9efc4ece2c2a03
Stephen North <snorth4@huskers.unl.edu>
2021-08-27 19:40:38 +0000

    initial commit

--- hello.c
+++ hello.c
@@ -1,18 +1,20 @@
 /**
  * Author: Trentyn Beekman
  * Date: 2021/08/26
- * 
+ *
  * This program will print my name.
- * 
+ *
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
      printf("Trentyn Beekman!\n");
      printf("Computer Engineering\n");
-     
+     printf("Stephen North\n");
+     printf("COMPUTER SCIENCE!\n");
+
      return 0;
-     
+
  }
